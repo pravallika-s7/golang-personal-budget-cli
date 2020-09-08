710a66c636f47f71290d5062dcd539e88bdabea7
pravallika-s7 <pravallika@s7works.io>
2020-09-08 18:03:41 +0530

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,15 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Max float32
+	Items []Item 
+}
+
 
 // Item stores item information
+
+type Item struct {
+	Description string
+	Price float32
+}
